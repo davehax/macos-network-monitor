c0cc54a36e360ebf3f9c877bb378b1a09f4c3510
davehax <dave_21_65@hotmail.com>
2018-02-03 18:14:05 +1100

    Initial Commit

---
+++ Network Monitor Status Bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Network Monitor Status Bar
+//
+//  Created by David Falconer on 3/2/18.
+//  Copyright © 2018 David Falconer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
